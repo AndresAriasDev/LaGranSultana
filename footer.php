c2c6be882e6d13e6a5c144732b821409923644e4
AndresAriasDev <andresariasdev02@gmail.com>
2025-10-13 17:35:35 -0600

    feat: se agrego el footer basico con info de cierre

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Footer del tema La Gran Sultana
+ *
+ * @package LaGranSultana
+ */
+?>
+
+<footer id="site-footer" class="bg-dark text-light py-6">
+    <div class="container mx-auto text-center">
+        <p>&copy; <?php echo date('Y'); ?> La Gran Sultana. Todos los derechos reservados.</p>
+        <p>Diseñado por tu equipo de desarrollo</p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
